85cc1fb03dbc7d6a16a18fa7228d4ed437e8234d
shraddha potekar <shraddhapotekar762@gmail.com>
2024-09-04 20:35:43 +0530

    name commit

--- src/Name/DisplayName.jsx
+++ src/Name/DisplayName.jsx
@@ -9,16 +9,21 @@ const DisplayName=()=>{
 
 
 const submitFn=(e)=>{
-    e.preventDefault();
-    setShowName(true);
+   e.preventDefault();
+   return true;
+}
+
+const validation=(nameVal)=>{
+const regex=/^[a-zA-Z]+$/
+ return regex.test(nameVal);
 }
 return(<div className={styles.mainDiv}><h1>Full Name Display</h1>
-<form onSubmit={(e)=>submitFn(e)}>
+<form onSubmit={(e)=>{if(submitFn(e)){ setShowName(true)}}}>
     <label htmlFor="name">First Name:</label>
-    <input type="text" id="name" onChange={(e)=>setName(e.target.value)} required/>
+    <input type="text" id="name" onChange={(e)=>{if(validation(e.target.value)){setName(e.target.value)}}} required/>
     <br />
     <label htmlFor="lName">Last Name:</label>
-    <input type="text" id="lName" onChange={(e)=>setSirName(e.target.value)} required/>
+    <input type="text" id="lName" onChange={(e)=>{if(validation(e.target.value)){setSirName(e.target.value)}}} required/>
     <br />
     <button type="submit">Submit</button>
 
