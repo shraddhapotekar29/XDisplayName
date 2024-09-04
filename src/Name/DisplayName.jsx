7f36d93c3da24ceac0eefd6d12441dd8fe6f5526
shraddha potekar <shraddhapotekar762@gmail.com>
2024-09-04 22:12:12 +0530

    stopwatch commit

--- src/Name/DisplayName.jsx
+++ src/Name/DisplayName.jsx
@@ -12,31 +12,31 @@ const submitFn=(e)=>{
    e.preventDefault();
 //    console.log(name);
 //    console.log(sirName);
-if (name && sirName) {
+if (name!=="" && sirName!=="") {
     setShowName(true);
   } 
 }
 
-const validation=(nameVal)=>{
-const regex=/^[a-zA-Z]+$/
- return regex.test(nameVal);
-}
+// const validation=(nameVal)=>{
+// const regex=/^[a-zA-Z]+$/
+//  return regex.test(nameVal);
+// }
 return(<div className={styles.mainDiv}><h1>Full Name Display</h1>
 <form onSubmit={submitFn}>
     <label htmlFor="name">First Name:</label>
     <input type="text" id="name" value={name} onChange={(e) => {
             const value = e.target.value;
-            if (validation(value)) {
+            // if (validation(value)) {
               setName(value);
-            }
+            // }
           }} required/>
     <br />
     <label htmlFor="lName">Last Name:</label>
     <input type="text" id="lName" value={sirName} onChange={(e) => {
             const value = e.target.value;
-            if (validation(value)) {
+            // if (validation(value)) {
               setSirName(value);
-            }
+            // }
           }} required/>
     <br />
     <button type="submit">Submit</button>
